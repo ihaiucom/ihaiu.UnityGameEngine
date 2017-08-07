23c7b72b4c5fae647bcaeffd58cba29631cb83c1
zengfeng <zengfeng@shinezone.com>
2017-08-07 22:43:12 +0800

    Login Panel

---
+++ UnityGame/Assets/Game/Editor/Achor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace com.ihaiu
+{
+	public class Achor 
+	{
+
+		[MenuItem("Game/Tool UI AchorSelf")]
+		[CanEditMultipleObjects]
+		public static void AchorSelf()
+		{
+			Transform[] list =  Selection.transforms;
+			foreach(Transform transform in list)
+			{
+				if(transform is RectTransform)
+				{
+					UIUtil.AchorSelf((RectTransform) transform);
+				}
+			}
+		}
+	}
+}

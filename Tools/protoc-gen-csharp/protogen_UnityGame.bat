7367d06f6e754a2ae8c765c81af5002226d6ff98
ihaiu <ihaiu@qq.com>
2017-08-22 01:28:53 +0800

    proton sharp

--- Tools/protoc-gen-csharp/protogen_UnityGame.bat
+++ Tools/protoc-gen-csharp/protogen_UnityGame.bat
@@ -1,4 +1,4 @@
-@protoc -I./../LuaProtoGen/protocol ../LuaProtoGen/protocol/hall.proto -oPackets.bin 
-clientgen\protogen.exe -i:Packets.bin -o:./../../UnityGame/Assets/Game/Scripts/GameGlobal/ProtoPacket.cs -ns:Games.PB -p:detectMissing
+@protoc -I./../../svn/proto ./../../svn/proto/*.proto -oPackets.bin 
+clientgen/protogen.exe -i:Packets.bin -o:./../../UnityGame/Assets/Plugins/Libs/ProtoPacket.cs -ns:Games.PB -p:detectMissing
 ::@handlegen ..\Packets.cs .. S ..\ProtoMap.cs
 ::@typemapgen ..\Packets.cs C ..\PacketTypeMap.cs
--- Tools/protoc-gen-csharp/protogen_UnityGame.sh
+++ Tools/protoc-gen-csharp/protogen_UnityGame.sh
@@ -1,2 +1,2 @@
-@protoc -I./../LuaProtoGen/protocol ../LuaProtoGen/protocol/hall.proto -oPackets.bin 
-mono ./clientgen/protogen.exe -i:Packets.bin -o:./../../UnityGame/Assets/Game/Scripts/GameGlobal/ProtoPacket.cs -ns:Games.PB -p:detectMissing
+@protoc -I./../../svn/proto ./../../svn/proto/*.proto -oPackets.bin 
+mono ./clientgen/protogen.exe -i:Packets.bin -o:./../../UnityGame/Assets/Plugins/Libs/ProtoPacket.cs -ns:Games.PB -p:detectMissing
